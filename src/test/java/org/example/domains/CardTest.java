44b80f95bd80ed4263742bb7266dbba86a7f3585
Quaicoe Richard <richardquaicoe78@gmail.com>
2021-11-25 06:17:41 +0000

    commented out entire test code

--- src/test/java/org/example/domains/CardTest.java
+++ src/test/java/org/example/domains/CardTest.java
@@ -1,31 +1,31 @@
-package org.example.domains;
+// package org.example.domains;
 
-import junit.framework.TestCase;
-import org.example.domains.enums.CardSuit;
-import org.example.domains.enums.CardValue;
-import org.junit.Before;
-import org.junit.Test;
+// import junit.framework.TestCase;
+// import org.example.domains.enums.CardSuit;
+// import org.example.domains.enums.CardValue;
+// import org.junit.Before;
+// import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+// import static org.junit.Assert.assertEquals;
 
-public class CardTest  {
+// public class CardTest  {
 
-    private Card card;
+//     private Card card;
 
-    @Before
-    public void  setup(){
-        card = new Card(CardValue.EIGHT, CardSuit.Club);
-    }
+//     @Before
+//     public void  setup(){
+//         card = new Card(CardValue.EIGHT, CardSuit.Club);
+//     }
 
-    @Test
-    public void testGetCardValue() {
-        int actual = card.getCardValue();
-        assertEquals(8,actual);
-    }
+//     @Test
+//     public void testGetCardValue() {
+//         int actual = card.getCardValue();
+//         assertEquals(8,actual);
+//     }
 
-    @Test
-    public void testGetCardSuit() {
-        CardSuit actual = card.getCardSuit();
-        assertEquals(CardSuit.Club,actual);
-    }
-}
+//     @Test
+//     public void testGetCardSuit() {
+//         CardSuit actual = card.getCardSuit();
+//         assertEquals(CardSuit.Club,actual);
+//     }
+// }
